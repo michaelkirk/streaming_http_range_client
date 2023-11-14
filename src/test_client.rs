c6df16d176c480664609c6c817a4a672f7253cad
Michael Kirk <michael.code@endoftheworl.de>
2023-11-14 13:19:16 +0100

    better debug output for test client

--- src/test_client.rs
+++ src/test_client.rs
@@ -1,19 +1,28 @@
 use crate::{empty, HttpClient, Reader, ReaderSource, ReqStats, Result};
 use async_trait::async_trait;
+use std::fmt::{Debug, Formatter};
 use std::ops::{Range, RangeFrom};
+
+pub(crate) struct ByteFormatter<'a>(pub &'a [u8]);
+
+impl Debug for ByteFormatter<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "0x{:02X?}", self.0)
+    }
+}
 
 /// Helpful for tests
 #[async_trait]
-impl ReaderSource for Vec<u8> {
+impl ReaderSource for LocalBytesClient {
     async fn get_byte_range(&self, range: Range<u64>) -> Result<Reader> {
         let usize_range = range.start as usize..range.end as usize;
-        let range_data = self[usize_range].to_owned();
+        let range_data = self.bytes[usize_range].to_owned();
         Ok(Box::pin(std::io::Cursor::new(range_data)))
     }
 
     async fn get_byte_range_from(&self, range: RangeFrom<u64>) -> Result<Reader> {
         let usize_range = range.start as usize..;
-        let range_data = self[usize_range].to_owned();
+        let range_data = self.bytes[usize_range].to_owned();
         Ok(Box::pin(std::io::Cursor::new(range_data)))
     }
 
@@ -22,10 +31,24 @@ impl ReaderSource for Vec<u8> {
     }
 }
 
+#[derive(Clone)]
+struct LocalBytesClient {
+    bytes: Vec<u8>,
+}
+impl Debug for LocalBytesClient {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("LocalBytesClient")
+            .field("bytes", &ByteFormatter(&self.bytes))
+            .finish()
+    }
+}
+
 impl HttpClient {
     pub fn test_client(data: &[u8]) -> Self {
         Self {
-            client: Box::new(data.to_vec()),
+            client: Box::new(LocalBytesClient {
+                bytes: data.to_vec(),
+            }),
             reader: empty(),
             pos: 0,
             range: None,
